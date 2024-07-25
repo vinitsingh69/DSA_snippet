654bf42f94a8275812d9444f5c20c759d468135c
Vinit Kumar <121727785+vinitsingh69@users.noreply.github.com>
2024-07-25 18:14:32 +0530

    Update Segment_Tree_Code_with_mik.cpp

--- Segment_Tree_Code_with_mik.cpp
+++ Segment_Tree_Code_with_mik.cpp
@@ -12,6 +12,22 @@ public:
         buildSegmentTree(2*i+2, mid+1, r, segmentTree, arr);
         segmentTree[i] = segmentTree[2*i + 1] + segmentTree[2*i + 2];
     }
+
+void updateSegTree(int index, int val, int i, int l, int r) {
+        if(l == r) {
+            segmentTree[i] = val;
+            return;
+        }
+
+        int mid = l + (r-l)/2;
+        if(index <= mid) {
+            updateSegTree(index, val, 2*i+1, l, mid);
+        } else {
+            updateSegTree(index, val, 2*i+2, mid+1, r);
+        }
+
+        segmentTree[i] = segmentTree[2*i+1] + segmentTree[2*i+2];
+    }
     
     int querySegmentTree(int start, int end, int i, int l, int r, vector<int>& segmentTree) {
         if(l > end || r < start) {
